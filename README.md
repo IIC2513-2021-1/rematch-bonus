242d14cdbe846c000a4232677d76ddda418c24a9
Sebastián Vicencio <sivicencio@gmail.com>
2021-07-09 11:51:58 -0400

    feat(project): initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Proyecto base actividad bonus REmatch
+
+Tendrás que utilizar este proyecto base para ejecutar el código correspondiente a esta actividad bonus.
+
+Lo primero es verificar las dependencias:
+- Node.js (v10 o v12)
+- Yarn
+
+Si estás usando `nvm`, entonces puedes seleccionar la versión de Node del proyecto con:
+```bash
+nvm use
+```
+(Si no, basta con que tengas instalada una de las versiones mencionadas más arriba)
+
+Instalamos las dependencias del proyecto:
+```bash
+yarn install
+```
+
+Y ya estamos listos para ejecutar el proyecto:
+```bash
+yarn start
+```
+
+Eso ejecutará el script `src/index.js` una sola vez, sin embargo, si quieres que un proceso escuche por cambios en los archivos, para que así se ejecute el script `src/index.js` cada vez que guardas algo, puedes ejecutar:
+```bash
+yarn dev
+```
+
+Para implementar tu solución, **sólo debes modificar** el valor de las variables `CONSULTAX`, con `X ∈ {1, ..., 7}`
+
+Cualquier duda, no dudes en preguntar en las [issues del proyecto](https://github.com/IIC2513-2021-1/projects/issues).
+
+### ¡Éxito!
---
+++ src/index.js
@@ -0,0 +1,144 @@
+const fs = require('fs');
+const path = require('path');
+const re = require('rematchjs');
+
+/*
+  ACTIVIDAD BONUS - REmatchJS y ciberataques
+
+  REmatchJS es una librería que sirve para extraer información a partir de
+  expresiones regulares. Pueden revisar un tutorial de como usarla en Node.js
+  siguiendo el siguiente link:
+      https://runkit.com/nicovsj/60e77d6f003217001a5d23a0
+
+  En esta actividad deberás extraer información a partir de un archivo de logs
+  de un servidor que ejecuta Apache:
+*/
+
+const TEXT = fs.readFileSync(path.resolve(__dirname, 'access.log'), 'utf8');
+/*
+  Cada línea del archivo presenta el siguiente formato:
+
+  {client ip address} - - {time of request} {client request} {status code} ...
+
+  Supongamos que deseamos extraer todos las direcciones IPs de aquellos clientes
+  que realizaron un request de tipo POST, junto con el tiempo en que se realizó
+  cada request. Dado que vamos a usar REmatchJS, debemos crear una consulta que
+  satisfaga el patrón que necesitamos.
+
+  Una posibilidad es el siguiente patrón:
+*/
+
+let consultaEjemplo = '(^|\\n)!x{[0-9.]+} - - \\[!y{[^\\n\\]]*}\\] "POST';
+
+/*
+  Analicemos este patrón por partes:
+
+  +'(^|\\n)':
+        Esto hara "match" con el inicio de una línea, esto porque '\\n' se
+        interpreta como un salto de línea y '^' es el inicio del documento.
+        Hay que notar que debemos colocar doble backslash '\\' para que JS lo
+        interprete como un backslash simple (dado que REmatchJS espera un '\n'
+        para un salto de línea).
+
+  +'!x{[0-9.]*}':
+        Acá le decimos a REmatchJS que queremos extraer la dirección  ip del
+        cliente en una variable "x". Dentro de los corchetes encontramos el
+        patrón '[0-9.]*' que hace match con un string que tiene dígitos o
+        puntos por caracteres (e.g. 192.168.0.1)
+
+  +'\\[!y{[^\\n\\]]*}\\]':
+        Esta parte puede resultar confusa, pero simplemente estamos extrayendo
+        los [dd/mm/yyyy:hh:MM:ss +tz]. Primero necesitamos hacer match con
+        un '[', el problema es que REmatchJS tiene ese caracter reservado para
+        su sintáxis (e.g. [0-9]), por lo que debemos escaparlo con '//[' en JS.
+        Lo mismo para ']': '\\]'. Así entonces estamos diciendo que vamos a
+        asociar a la variable "y" cualquier cosa que no sea un ']' o un '\n',
+        que esté dentro de dos corchetes '[ ]' (i.e. '[^\\n\\]]*')
+
+  +' "POST':
+        Esta parte de la consulta obliga que la request sea tipo POST.
+
+  Ahora ya con la consulta escrita, podemos revisar los resultados con REmatchJS:
+*/
+
+let rgx = re.compile(consultaEjemplo);
+
+for (let match of rgx.findIter(TEXT)) {
+  let result = match.groupdict();
+  // console.log(result);
+}
+
+/*
+  En la variable "result" se entregará un objeto que tendrá por llave cada
+  variable presente en la consulta, y su resultado asociado. Puedes probar
+  imprimiendo la variable "result" para ver que funciona.
+*/
+
+/*
+  CONSULTAS SIMPLES SOBRE LOGS:
+  Pedimos que modifiquen los valores de las siguientes variables
+  acorde a lo que se pide por enunciado.
+*/
+
+// Extrer todas las líneas con requests tipo POST:
+const CONSULTA1 = '';
+
+// Extraer sólo la dirección IP en los requests tipo GET:
+const CONSULTA2 = '';
+
+// Extraer sólo las requests donde el servidor entrega código NOT FOUND (404):
+const CONSULTA3 = '';
+
+// Extraer IP de todos los requests que se redirigieron (código 302) y hacia dónde se
+// redirigieron (asumir que es la 2da url):
+const CONSULTA4 = '';
+
+/* 
+  IDENTIFICAR UN POSIBLE ATAQUE AL SERVIDOR:
+
+  Suponga que los logs que actualmente ud. está analizando corresponden a un
+  servidor que sufrió un ataque cibernético. Lo único que ud. sabe es que
+  el atacante desde una única dirección IP adquirió acceso de administrador
+  al sitio de alguna manera.
+*/
+
+// Entregue una consulta que busque aquellas direcciones IPs que realizaron
+// un request que contenga la palabra "admin" .
+const CONSULTA5 = '';
+
+// Si hace lo anterior de manera correcta, encontrará varias direcciones IPs.
+// Necesitamos otra forma entonces de identificar al atacante. Intente entonces
+// buscar todas las direcciones IPs que realizaron una request que contenga
+// la palabra "SELECT".
+// La idea es identificar si acaso el atacante obtuvo credenciales a partir de
+// una SQL Injection.
+const CONSULTA6 = '';
+
+// Si realizó lo anterior con éxito, ya debería saber cuál es la dirección IP
+// del atacante. Obtenga así entonces todas las líneas del archivo log
+// correspondientes a requests realizados por el atacante
+const CONSULTA7 = '';
+
+// FUNCIÓN PARA OBTENER EL OBJETO CON TODOS LOS RESULTADOS DE LAS CONSULTAS
+// que será utilizado en la corrección:
+
+const arrayConsultas = [
+  CONSULTA1,
+  CONSULTA2,
+  CONSULTA3,
+  CONSULTA4,
+  CONSULTA5,
+  CONSULTA6,
+  CONSULTA7,
+];
+
+const getAllOutputs = (queriesArray) => {
+  let result = new Object();
+  for (let i = 0; i < queriesArray.length; i++) {
+    let rgx = re.compile(queriesArray[i]);
+    result[i + 1] = rgx.findall(TEXT);
+  }
+  return result;
+};
+
+getAllOutputs(arrayConsultas);
